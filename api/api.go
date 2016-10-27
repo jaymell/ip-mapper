059fe87f80081878dd0a1fcd720a19c9ed71e25e
James Lucas <jamesmlucas@gmail.com>
2016-10-27 06:26:38 +0000

    cache stuff in progress

--- api/api.go
+++ api/api.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-    "encoding/json"
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
@@ -18,26 +18,38 @@ import (
 type Data interface{}
 
 type APIConfig struct {
-    DataURL        string `json: "DataURL"`
-    CollectionName string `json: "CollectionName"`
-    IPGeolocator map[string]string `json: "IPGeolocator"`
+	DataURL        string            `json: "DataURL"`
+	CollectionName string            `json: "CollectionName"`
+	IPGeolocator   map[string]string `json: "IPGeolocator"`
+	Cache          map[string]string `json: "Cache"`
 }
 
 // stores persistent stuff needed for API, e.g., geolocation object, db handle
 type API struct {
-	config *APIConfig
-	routes []*daemon.Command
+	config       *APIConfig
+	routes       []*daemon.Command
 	ipGeolocator IPGeolocator
+	cache        Cache
 }
 
 type IPLocation struct {
-	Latitude float64 `json: "latitude"`
-	Longitude float64 `json: "longitude"`
-	CountryCode string `json: country_iso"`
+	Latitude    float64 `json: "latitude"`
+	Longitude   float64 `json: "longitude"`
+	CountryCode string  `json: country_iso"`
 }
 
 type IPGeolocator interface {
 	IPLocation(ip string) (*IPLocation, error)
+}
+
+type Cache interface {
+	GetCache(ip string) *IPLocation
+	PutCache(ip string) error
+}
+
+type MongoSession struct {
+	session *mgo.Session
+	col     string
 }
 
 type MaxMindIPGeolocator struct {
@@ -65,18 +77,18 @@ func (gl *MaxMindIPGeolocator) ipLocation(ip string) (*geoip2.Response, error) {
 
 func (gl *MaxMindIPGeolocator) IPLocation(ip string) (*IPLocation, error) {
 
-	// TODO: check IP cache before calling the 
+	// TODO: check IP cache before calling the
 	// api
 
 	resp, err := gl.ipLocation(ip)
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
 	// convert to vendor-generic type for returnage:
 	location := IPLocation{
-		Latitude: resp.Location.Latitude,
-		Longitude: resp.Location.Longitude,
+		Latitude:    resp.Location.Latitude,
+		Longitude:   resp.Location.Longitude,
 		CountryCode: resp.Country.IsoCode,
 	}
 
@@ -93,35 +105,81 @@ func (api *API) addRoute(cmd *daemon.Command) {
 }
 
 func (api *API) loadConfig(f *os.File) error {
-    decoder := json.NewDecoder(f)
-    config := APIConfig{}
+	decoder := json.NewDecoder(f)
+	config := APIConfig{}
 
-    err := decoder.Decode(&config)
-    if err != nil {
-        return fmt.Errorf("unable to decode json: ", err)
-    }
+	err := decoder.Decode(&config)
+	if err != nil {
+		return fmt.Errorf("unable to decode json: ", err)
+	}
 
-    api.config = &config
+	api.config = &config
 
-    return nil
+	return nil
 }
 
 func (api *API) loadIPGeolocator() error {
 
-    geolocator := api.config.IPGeolocator["Vendor"]
-    switch geolocator {
-    case "MaxMind":
-        maxMindUserID := api.config.IPGeolocator["MaxMindUserID"]
-        maxMindLicenseKey := api.config.IPGeolocator["MaxMindLicenseKey"]
+	geolocator := api.config.IPGeolocator["Vendor"]
+	switch geolocator {
+	case "MaxMind":
+		maxMindUserID := api.config.IPGeolocator["MaxMindUserID"]
+		maxMindLicenseKey := api.config.IPGeolocator["MaxMindLicenseKey"]
 		maxMindIPGeolocator, err := newMaxMind(maxMindUserID, maxMindLicenseKey)
 		if err != nil {
 			return err
 		}
 		api.ipGeolocator = maxMindIPGeolocator
 		return nil
-    default:
+	default:
 		return fmt.Errorf("No useable ip geolocation vendor specified")
-    }
+	}
+}
+
+func getMongoSession(url string) (*MongoSession, error) {
+
+	dialInfo, err := mgo.ParseURL(url)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse db url: ", err)
+	}
+
+	dialInfo.Timeout = time.Duration(5) * time.Second
+
+	session, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to db: ", err)
+	}
+
+	return &MongoSession{
+		session: session,
+		col:     col,
+	}, nil
+
+}
+
+func (api *API) loadCache() error {
+	cache := api.config.Cache["Vendor"]
+	switch cache.ToLower() {
+	case "mongo":
+		url := api.config.Cache["URL"]
+		col := api.config.Cache["Collection"]
+		session, err := getMongoSession(url)
+		if err != nil {
+			return nil, err
+		}
+		api.cache = session
+		return nil
+	}
+}
+
+func (m *MongoSession) GetCache(ip string) *IPLocation {
+	// TODO
+	return nil
+}
+
+func (m *MongoSession) PutCache(ip string) error {
+	// TODO
+	return nil
 }
 
 func New(f *os.File) (*API, error) {
@@ -129,16 +187,22 @@ func New(f *os.File) (*API, error) {
 	api := &API{}
 
 	// load config
-    err := api.loadConfig(f)
-    if err != nil {
-        return nil, err
-    }
+	err := api.loadConfig(f)
+	if err != nil {
+		return nil, err
+	}
 
 	// initialize geolocator
 	err = api.loadIPGeolocator()
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
+
+	// load cache
+	err = api.loadCache()
+	if err != nil {
+		return nil, err
+	}
 
 	// connect routes with their handlers
 	var (
@@ -160,7 +224,6 @@ func New(f *os.File) (*API, error) {
 	for _, c := range apiRoutes {
 		api.addRoute(c)
 	}
-
 
 	return api, nil
 
@@ -194,6 +257,7 @@ func (api *API) getData() (interface{}, error) {
 	}
 }
 
+// FIXME -- make this get session using getMongoSession function:
 func getMongoData(URL string, col string) (interface{}, error) {
 	dialInfo, err := mgo.ParseURL(URL)
 	if err != nil {
@@ -206,10 +270,11 @@ func getMongoData(URL string, col string) (interface{}, error) {
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to db: ", err)
 	}
+
 	c := session.DB(dialInfo.Database).C(col)
 	results := []Data{}
 	c.Find(nil).All(&results)
-	// FIXME -- don't always return nil for err, probably:
+
 	return results, nil
 }
 
@@ -221,28 +286,36 @@ func (api *API) getIPLocation(c *daemon.Command, r *http.Request) daemon.Respons
 
 	ip := r.URL.Query().Get("ip")
 	if ip == "" {
-        return &daemon.Resp{
-            Status: http.StatusBadRequest,
-            Result: nil,
-        }
+		return &daemon.Resp{
+			Status: http.StatusBadRequest,
+			Result: nil,
+		}
 	}
 
 	// validate an ip address was actually passed:
 	validIP := net.ParseIP(ip)
 	if validIP == nil {
-        return &daemon.Resp{
-            Status: http.StatusBadRequest,
-            Result: nil,
-        }
+		return &daemon.Resp{
+			Status: http.StatusBadRequest,
+			Result: nil,
+		}
 	}
 
+	// TODO: check cache
 	ipLocation, err := api.ipGeolocator.IPLocation(ip)
 	if err != nil {
-        return &daemon.Resp{
-            Status: http.StatusInternalServerError,
-            Result: nil,
-        }
+		return &daemon.Resp{
+			Status: http.StatusInternalServerError,
+			Result: nil,
+		}
 
 	}
+	// TODO: insert into cache
 	return daemon.SyncResponse(ipLocation)
+}
+
+func (api *API) getCache(ip) *IPLocation {
+
+	//
+
 }
