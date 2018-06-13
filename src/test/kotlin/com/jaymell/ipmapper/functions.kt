e3dc831f0aaae650ee2c96f4976c30291e9834c1
James Lucas <jamesmlucas@gmail.com>
2018-06-13 01:13:24 -0500

    test before/after working with autowired template

--- src/test/kotlin/com/jaymell/ipmapper/functions.kt
+++ src/test/kotlin/com/jaymell/ipmapper/functions.kt
@@ -4,44 +4,43 @@ import org.junit.jupiter.api.BeforeAll
 import java.time.LocalDateTime
 import com.mongodb.MongoClient
 import org.assertj.core.data.Offset
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.ZoneOffset
 
 
 data class TestItem(val Date: LocalDateTime, val testKey: String)
 
-@Configuration
-class SpringMongoConfig {
-
-    @Bean
-    @Throws(Exception::class)
-    fun mongoTemplate(): MongoTemplate {
-
-        return MongoTemplate(MongoClient("127.0.0.1"), "test")
-
-    }
-
-}
-
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
 class TestMongoDateQuery {
 
-//    @Autowired
-//    private lateinit var template: MongoTemplate
+    @Autowired
+    private lateinit var template: MongoTemplate
 
+    val dbName = "testing"
     val colName = "test"
 
     @BeforeAll
     fun insertTestDbItems() {
-        val template = MongoTemplate(MongoClient("127.0.0.1"), "james")
         template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
         template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
         template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
         template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
     }
+
+    @AfterAll
+    fun cleanUp() {
+        template.dropCollection(colName)
+    }
+
     @Test
     fun alwaysPassTheTest() {
         println("passed")
