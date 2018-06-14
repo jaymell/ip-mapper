2d3eb0e065594c484ddb55f9fd5389b93d933f54
James Lucas <jamesmlucas@gmail.com>
2018-06-14 00:47:04 -0500

    start adding real tests

--- src/test/kotlin/com/jaymell/ipmapper/functions.kt
+++ src/test/kotlin/com/jaymell/ipmapper/functions.kt
@@ -3,6 +3,7 @@ package com.jaymell.ipmapper
 import org.junit.jupiter.api.BeforeAll
 import java.time.LocalDateTime
 import com.mongodb.MongoClient
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
@@ -16,7 +17,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.ZoneOffset
 
 
-data class TestItem(val Date: LocalDateTime, val testKey: String)
+data class TestItem(val date: LocalDateTime, val testKey: String)
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
@@ -25,15 +26,14 @@ class TestMongoDateQuery {
     @Autowired
     private lateinit var template: MongoTemplate
 
-    val dbName = "testing"
     val colName = "test"
 
     @BeforeAll
     fun insertTestDbItems() {
         template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
-        template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
-        template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
-        template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC), "testValue1"), colName)
+        template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC).minusHours(1), "testValue2"), colName)
+        template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC).minusDays(2), "testValue3"), colName)
+        template.insert(TestItem(LocalDateTime.now(ZoneOffset.UTC).minusYears(1), "testValue4"), colName)
     }
 
     @AfterAll
@@ -42,7 +42,25 @@ class TestMongoDateQuery {
     }
 
     @Test
-    fun alwaysPassTheTest() {
-        println("passed")
+    fun should_return_results_from_last_24_hours_if_no_params_passed() {
+        val results = queryMongoByDate(template, colName, null, null)
+                .asSequence()
+                .toList()
+        assertThat(results.size).isEqualTo(2)
+    }
+
+    @Test
+    fun should_return_expected_results_if_only_lte_param_is_passed() {
+
+    }
+
+    @Test
+    fun should_return_expected_results_if_only_gte_param_is_passed() {
+
+    }
+
+    @Test
+    fun should_return_expected_results_if_both_lte_and_gte_are_passed() {
+
     }
 }
