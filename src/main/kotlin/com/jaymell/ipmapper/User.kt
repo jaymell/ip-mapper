839afad21e8857faaaaa422531096b4055c62260
James Lucas <jamesmlucas@gmail.com>
2018-07-31 01:02:34 -0500

    dont allow duplicate users, make name _id

--- src/main/kotlin/com/jaymell/ipmapper/User.kt
+++ src/main/kotlin/com/jaymell/ipmapper/User.kt
@@ -2,11 +2,12 @@ package com.jaymell.ipmapper
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.repository.NoRepositoryBean
 
-data class User(val name: String, var password: String)
+class User(@Id val name: String, var password: String)
 
 interface UserRepository : MongoRepository<User, String> {
     fun findByName(name: String): User?
--- src/main/kotlin/com/jaymell/ipmapper/UserController.kt
+++ src/main/kotlin/com/jaymell/ipmapper/UserController.kt
@@ -1,7 +1,10 @@
 package com.jaymell.ipmapper
 
+import mu.KLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,9 +17,17 @@ class UserController(
         val userRepository: UserRepository,
         val bCryptPasswordEncoder: BCryptPasswordEncoder) {
 
+    companion object : KLogging()
+
     @PostMapping("/create")
-    fun create(@RequestBody user: User) {
+    fun create(@RequestBody user: User): ResponseEntity<HttpStatus> {
+        val u: User? = userRepository.findByName(user.name)
+        u.let {
+            logger.info("Duplicate user creation attempted")
+            return ResponseEntity(HttpStatus.CONFLICT)
+        }
         user.password = bCryptPasswordEncoder.encode(user.password)
         userRepository.save(user)
+        return ResponseEntity(HttpStatus.OK)
     }
 }
