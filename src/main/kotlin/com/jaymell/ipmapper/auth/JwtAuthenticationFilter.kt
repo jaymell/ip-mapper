d8f86136dfceabf536f488179b2c522e86fe2860
James Lucas <jamesmlucas@gmail.com>
2018-08-04 15:04:40 -0500

    derp

--- src/main/kotlin/com/jaymell/ipmapper/auth/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/jaymell/ipmapper/auth/JwtAuthenticationFilter.kt
@@ -1,6 +1,7 @@
 package com.jaymell.ipmapper
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.security.authentication.AuthenticationManager
@@ -13,19 +14,27 @@ import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import com.jaymell.ipmapper.securityconstants.*
+import mu.KLogging
 
 class JwtAuthenticationFilter(private val authManager: AuthenticationManager)
     : UsernamePasswordAuthenticationFilter() {
 
+    companion object : KLogging()
+
     override fun attemptAuthentication(request: HttpServletRequest?, response: HttpServletResponse?): Authentication {
-		println("this request: $request")
         try {
-            val creds: User = ObjectMapper()
-                    .readValue(request?.inputStream, User::class.java)
+            val mapper = jacksonObjectMapper()
+            val creds: User = mapper
+                    .readValue(request?.inputStream?.bufferedReader().use { it?.readText()}!!)
+            logger.debug("Creds: ${creds}")
+            logger.debug("Creds name: ${creds.name}")
+            logger.debug("Creds pass: ${creds.password}")
+            // this is null:
             return authenticationManager.authenticate(
                     UsernamePasswordAuthenticationToken(creds.name, creds.password, ArrayList())
             )
         } catch (e: IOException) {
+            logger.debug("$e")
             throw RuntimeException()
         }
     }
